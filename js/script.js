a8f30e43e555c696a6d8f543a27db03733e0f286
removedporn <86824510+removedporn@users.noreply.github.com>
2021-07-27 20:34:40 +0800

    Delete js directory

--- js/script.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.open').addEventListener('click', () => {
-    document.querySelector(".nav-list").classList.add('active');
-})
-
-document.querySelector('.close').addEventListener('click', () => {
-    document.querySelector('.nav-list').classList.remove('active');
-})
